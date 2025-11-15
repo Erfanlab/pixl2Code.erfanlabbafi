620e3b3c0b58822df5866851255996873820fd12
erfanlabbafi <erfanlab2000@gmail.com>
2025-11-15 23:40:11 +0330

    add animation for change logo and improve change function

---
+++ src/environments/environment.stage.ts
@@ -0,0 +1,7 @@
+import { Environment } from './environment.interface';
+
+export const environment: Environment = {
+  production: false,
+  apiUrl: 'https://api.stage.com',
+  appUrl: 'https://app.stage.com'
+};
